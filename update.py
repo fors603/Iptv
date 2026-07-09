3e255c4b1b402fb6629a19e2fe1a4f1580840f83
fors603 <ramiforsar@gmail.com>
2026-07-09 00:41:14 -0700

    Delete update.py

--- update.py
+++
@@ -1,6 +0,0 @@
-with open("playlist.m3u", "w") as f:
-    f.write("#EXTM3U\n")
-    f.write("#EXTINF:-1, Updated by GitHub\n")
-    f.write("http://example.com/test\n")
-
-print("Playlist updated.")
